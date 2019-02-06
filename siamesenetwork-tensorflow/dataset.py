484da7bb6ea77b944e29e748068a63f385def56b
sasakishun <Pakka-xeno@keio.jp>
2019-02-06 23:57:16 +0900

    add siamesenet

---
+++ siamesenetwork-tensorflow/dataset.py
@@ -0,0 +1,73 @@
+from __future__ import generators, division, absolute_import, with_statement, print_function, unicode_literals
+
+import numpy as np
+import matplotlib.pyplot as plt
+from tensorflow.keras.datasets import mnist
+
+
+class Dataset(object):
+    images_train = np.array([])
+    images_test = np.array([])
+    labels_train = np.array([])
+    labels_test = np.array([])
+    unique_train_label = np.array([])
+    map_train_label_indices = dict()
+
+    def _get_siamese_similar_pair(self):
+        label = np.random.choice(self.unique_train_label)
+        l, r = np.random.choice(self.map_train_label_indices[label], 2, replace=False)
+        return l, r, 1
+
+    def _get_siamese_dissimilar_pair(self):
+        label_l, label_r = np.random.choice(self.unique_train_label, 2, replace=False)
+        l = np.random.choice(self.map_train_label_indices[label_l])
+        r = np.random.choice(self.map_train_label_indices[label_r])
+        return l, r, 0
+
+    def _get_siamese_pair(self):
+        if np.random.random() < 0.5:
+            return self._get_siamese_similar_pair()
+        else:
+            return self._get_siamese_dissimilar_pair()
+
+    def get_siamese_batch(self, n):
+        idxs_left, idxs_right, labels = [], [], []
+        for _ in range(n):
+            l, r, x = self._get_siamese_pair()
+            idxs_left.append(l)
+            idxs_right.append(r)
+            labels.append(x)
+        return self.images_train[idxs_left, :], self.images_train[idxs_right, :], np.expand_dims(labels, axis=1)
+
+
+class MNISTDataset(Dataset):
+    def __init__(self):
+        print("===Loading MNIST Dataset===")
+        (self.images_train, self.labels_train), (self.images_test, self.labels_test) = mnist.load_data()
+        self.images_train = np.expand_dims(self.images_train, axis=3) / 255.0
+        self.images_test = np.expand_dims(self.images_test, axis=3) / 255.0
+        self.labels_train = np.expand_dims(self.labels_train, axis=1)
+        self.unique_train_label = np.unique(self.labels_train)
+        self.map_train_label_indices = {label: np.flatnonzero(self.labels_train == label) for label in
+                                        self.unique_train_label}
+        print("Images train :", self.images_train.shape)
+        print("Labels train :", self.labels_train.shape)
+        print("Images test  :", self.images_test.shape)
+        print("Labels test  :", self.labels_test.shape)
+        print("Unique label :", self.unique_train_label)
+
+    # print("Map label indices:", self.map_train_label_indices)
+
+
+if __name__ == "__main__":
+    # Test if it can load the dataset properly or not. use the train.py to run the training
+    a = MNISTDataset()
+    batch_size = 4
+    ls, rs, xs = a.get_siamese_batch(batch_size)
+    f, axarr = plt.subplots(batch_size, 2)
+    for idx, (l, r, x) in enumerate(zip(ls, rs, xs)):
+        print("Row", idx, "Label:", "similar" if x else "dissimilar")
+        print("max:", np.squeeze(l, axis=2).max())
+        axarr[idx, 0].imshow(np.squeeze(l, axis=2))
+        axarr[idx, 1].imshow(np.squeeze(r, axis=2))
+    plt.show()
---
+++ siamesenetwork-tensorflow/model.py
@@ -0,0 +1,55 @@
+from __future__ import generators, division, absolute_import, with_statement, print_function, unicode_literals
+
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+import tensorflow.contrib.slim as slim
+
+flags = tf.app.flags
+FLAGS = flags.FLAGS
+
+
+def mnist_model(input, reuse=False):
+    with tf.name_scope("model"):
+        with tf.variable_scope("conv1") as scope:
+            net = tf.contrib.layers.conv2d(input, 32, [7, 7], activation_fn=tf.nn.relu, padding='SAME',
+                                           weights_initializer=tf.contrib.layers.xavier_initializer_conv2d(),
+                                           scope=scope, reuse=reuse)
+            net = tf.contrib.layers.max_pool2d(net, [2, 2], padding='SAME')
+
+        with tf.variable_scope("conv2") as scope:
+            net = tf.contrib.layers.conv2d(net, 64, [5, 5], activation_fn=tf.nn.relu, padding='SAME',
+                                           weights_initializer=tf.contrib.layers.xavier_initializer_conv2d(),
+                                           scope=scope, reuse=reuse)
+            net = tf.contrib.layers.max_pool2d(net, [2, 2], padding='SAME')
+
+        with tf.variable_scope("conv3") as scope:
+            net = tf.contrib.layers.conv2d(net, 128, [3, 3], activation_fn=tf.nn.relu, padding='SAME',
+                                           weights_initializer=tf.contrib.layers.xavier_initializer_conv2d(),
+                                           scope=scope, reuse=reuse)
+            net = tf.contrib.layers.max_pool2d(net, [2, 2], padding='SAME')
+
+        with tf.variable_scope("conv4") as scope:
+            net = tf.contrib.layers.conv2d(net, 256, [1, 1], activation_fn=tf.nn.relu, padding='SAME',
+                                           weights_initializer=tf.contrib.layers.xavier_initializer_conv2d(),
+                                           scope=scope, reuse=reuse)
+            net = tf.contrib.layers.max_pool2d(net, [2, 2], padding='SAME')
+
+        with tf.variable_scope("conv5") as scope:
+            net = tf.contrib.layers.conv2d(net, 2, [1, 1], activation_fn=None, padding='SAME',
+                                           weights_initializer=tf.contrib.layers.xavier_initializer_conv2d(),
+                                           scope=scope, reuse=reuse)
+            net = tf.contrib.layers.max_pool2d(net, [2, 2], padding='SAME')
+
+        net = tf.contrib.layers.flatten(net)
+
+    return net
+
+
+def contrastive_loss(model1, model2, y, margin):
+    with tf.name_scope("contrastive-loss"):
+        distance = tf.sqrt(tf.reduce_sum(tf.pow(model1 - model2, 2), 1, keepdims=True))
+        similarity = y * tf.square(distance)  # keep the similar label (1) close to each other
+        dissimilarity = (1 - y) * tf.square(tf.maximum((margin - distance), 0))
+        # give penalty to dissimilar label if the distance is bigger than margin
+        return tf.reduce_mean(dissimilarity + similarity) / 2
