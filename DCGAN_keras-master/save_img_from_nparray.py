37065e3b1cb8e3293e33d32863b195d9745d31a2
sasakishun <Pakka-xeno@keio.jp>
2019-10-30 17:33:38 +0900

    update

--- DCGAN_keras-master/save_img_from_nparray.py
+++ DCGAN_keras-master/save_img_from_nparray.py
@@ -5,6 +5,7 @@ import os
 from datetime import datetime
 import cv2
 
+
 class SaveImgFromList:
     def __init__(self, imgs, shape, tag=None, comment=None):
         self.imgs = [np.array(img) * 255. for img in imgs]
@@ -12,37 +13,56 @@ class SaveImgFromList:
         self.tag = tag
         if comment is not None:
             comment = "_" + comment
-        self.path = os.getcwd() + r"\corrected_img\{}.jpg".format(datetime.now().strftime("%Y%m%d%H%M%S")+comment)
+        self.path = os.getcwd() + r"\corrected_img\{}.jpg".format(datetime.now().strftime("%Y%m%d%H%M%S") + comment)
 
     def __call__(self, *args, **kwargs):
-        if self.shape[0] == 1  or self.shape[1] == 1:
+        if self.shape[0] == 1 or self.shape[1] == 1:
             return
         imgs = [Image.fromarray(np.reshape(img, self.shape)).convert('RGB') for img in self.imgs]
-        fig = plt.figure(figsize=(6, 8*len(imgs)))
+        fig = plt.figure(figsize=(6, 8 * len(imgs)))
         print(imgs)
         for i, p in enumerate(imgs):
-            ax = fig.add_subplot(1, len(imgs)+1, i+1)
-            ax.imshow(imgs[i])# , interpolation=p)
+            ax = fig.add_subplot(1, len(imgs) + 1, i + 1)
+            ax.imshow(imgs[i])  # , interpolation=p)
             ax.set_axis_off()
             ax.set_title(self.tag[i])
 
         # 一番最初の画像と修正後の画像の変更箇所を明示
         prev = self.imgs[0]
         corrected = self.imgs[-1]
-        diff = np.abs(np.subtract(corrected,  prev))
-        ax = fig.add_subplot(1, len(imgs)+1, len(imgs) + 1)
-        ax.imshow(Image.fromarray(np.reshape(diff, self.shape)).convert('RGB'))
+        ax = fig.add_subplot(1, len(imgs) + 1, len(imgs) + 1)
+        ax.imshow(Image.fromarray(colorize_pixel_diff(corrected, prev, self.shape)))
         ax.set_axis_off()
         ax.set_title("diff")
         plt.savefig(self.path, bbox_inches="tight", pad_inches=0.1)
         plt.close()
         print("saved to -> {}".format(self.path))
 
+
+def colorize_pixel_diff(corrected, prev, shape):
+    _diff = np.subtract(corrected, prev)
+    diff = np.zeros((shape[0], shape[1], 3))
+    for i in range(_diff.shape[0]):
+        for j in range(_diff.shape[1]):
+            if _diff[i, j] > 0:
+                diff[i, j, 0] = _diff[i, j]
+            elif _diff[i, j] < 0:
+                diff[i, j, 2] = -_diff[i, j]
+    # print("_diff:{}".format(_diff))
+    # print("diff:{}".format(diff))
+    return np.array(diff).astype(np.uint8)
+
+
 import numpy as np
+
 if __name__ == '__main__':
-    array = np.ones((28, 28))
-    SaveImgFromList([array] + [array] + [array] + [array],
-                    (28, 28),
-                    tag=["original[{}]".format(1)] +
-                        ["corrected[{}]".format(2) for _ in range(4)],
+    shape = (8, 8)
+    array = np.zeros(shape)
+    array[1, 1] = 1
+    corrected = np.zeros(shape)
+    corrected[0, 0] = 1
+    SaveImgFromList([array] + [array] + [array] + [corrected],
+                    shape,
+                    tag=["ori[{}]".format(1)] +
+                        ["cor[{}]".format(2) for _ in range(4)],
                     comment="corrected_class[{}]_sample[{}]".format(0, 0))()
