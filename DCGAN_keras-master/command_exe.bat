daa9c846164ecb77f628778ef9d8c8921a475be9
DESKTOP-MO312VG\papap <pakka-xeno@keio.jp>
2019-11-09 20:30:12 +0900

    update

--- DCGAN_keras-master/command_exe.bat
+++ DCGAN_keras-master/command_exe.bat
@@ -2,11 +2,11 @@ for /L %%i in (1,1,10) do (
 cls
 python binary__tree_main.py --iris --train --child_num 2
 python binary__tree_main.py --iris --test --child_num 2
-python binary__tree_main.py --iris --train --child_num 2 --shrinked --adversarial
+python binary__tree_main.py --iris --test --child_num 2 --shrinked --adversarial
 
 python binary__tree_main.py --wine --train --child_num 2
 python binary__tree_main.py --wine --test --child_num 2
-python binary__tree_main.py --wine --train --child_num 2 --shrinked --adversarial
+python binary__tree_main.py --wine --test --child_num 2 --shrinked --adversarial
 
 python binary__tree_main.py --digit --train --child_num 3 --is_image
 python binary__tree_main.py --digit --test --child_num 3 --is_image
